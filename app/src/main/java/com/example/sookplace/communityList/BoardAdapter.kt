afb9d471dd1267fca714d60a73e77833761cd103
kyr511 <yerin2k4@gmail.com>
2025-09-28 17:00:44 +0900

    community_rv_item.xml 구현

---
+++ app/src/main/java/com/example/sookplace/communityList/BoardAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.sookplace.communityList
+
+class BoardAdapter {
+}
---
+++ app/src/main/java/com/example/sookplace/communityList/BoardModel.kt
@@ -0,0 +1,8 @@
+package com.example.sookplace.communityList
+
+data class BoardModel(
+    val uid: String = "",
+    val title: String = "",
+    val contant: String = "",
+    val time: String = ""
+)
