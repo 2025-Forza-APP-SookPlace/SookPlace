a9d41586e94ab1d07b1452f14f7fc8d393c3edf6
kyr511 <yerin2k4@gmail.com>
2025-10-10 15:27:48 +0900

    Initialize master branch

--- app/src/main/java/com/example/sookplace/communityList/BoardAdapter.kt
+++
@@ -1,4 +0,0 @@
-package com.example.sookplace.communityList
-
-class BoardAdapter {
-}
--- app/src/main/java/com/example/sookplace/communityList/BoardModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sookplace.communityList
-
-data class BoardModel(
-    val uid: String = "",
-    val title: String = "",
-    val contant: String = "",
-    val time: String = ""
-)
