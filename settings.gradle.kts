3e4fffc2ec34db60970bf0f567e3a93e9db252d5
gimminseo <gminseo012@gmail.com>
2025-11-22 22:28:44 +0900

    MyPage and Map

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://navercorp.jfrog.io/artifactory/maven/") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://repository.map.naver.com/archive/maven")
+    }
+}
+
+rootProject.name = "MyPage"
+include(":app")
